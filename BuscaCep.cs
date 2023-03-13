b4dcc358754457e6e0c0927e14a4a1fc1f773e2a
Gabriel Cavalcante Brandão <112448816+GabrielCBrandao@users.noreply.github.com>
2023-03-13 17:46:11 -0300

    Add files via upload

---
+++ BuscaCep.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using TesteCorreios.Core;
+
+namespace TesteCorreios
+{
+    class BuscaCep : Begin
+    {
+        [Test]
+        public void Test()
+        {
+            driver.FindElement(By.Id("endereco")).SendKeys("25963130");
+            driver.FindElement(By.Id("btn_pesquisar")).Click();
+            Assert.That(driver.FindElement(By.XPath("//*[@id=\'resultado-DNEC\']/tbody/tr/td[1]")).Text, Does.Contain("Rua Armando Farjado"));
+            
+
+
+
+
+        }
+    }
+}
---
+++ RandomDataTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteCorreios.Core;
+
+namespace TesteCorreios
+{
+    internal class RandomDataTests : Begin
+    {
+        [Test]
+        public void RandomData()
+        {
+            string nome = GeraNomeAleatorio();
+            Console.WriteLine("Nome completo: " + nome);
+            string email = GeraEmailAleatorio();
+            Console.WriteLine("Email: " + email);
+            string dataNasc = GeraDataNascimento();
+            Console.WriteLine("Data de nascimento: " + dataNasc);
+            string celNumber = GeraCelular();
+            Console.WriteLine("Celular: " + celNumber);
+        }
+        [Test]
+        public void PreencheCadastroAmazonPrime()
+        {
+            string nomeCompleto = GeraNomeAleatorio();
+            EscreveTexto("//input[@id='ap_customer_name']", nomeCompleto);
+
+            string celular = GeraCelular();
+            EscreveTexto("//input[@id='ap_email']", celular);
+
+            string senha = GeraCelular();
+            EscreveTexto("//input[@id='ap_password']", senha);
+
+            string senha2 = GeraCelular();
+            EscreveTexto("//input[@id='ap_password_check']", senha2);
+
+            ClicaElemento("//input[@id='continue']");
+        }
+    }
+}
